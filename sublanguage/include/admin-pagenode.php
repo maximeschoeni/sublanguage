2fddbd50fe098e3d6178ca95fae39386940c0a24
maximeschoeni <maximeschoeni@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-03 18:33:31 +0000

    my first transfer
    git-svn-id: https://plugins.svn.wordpress.org/sublanguage/trunk@1192005 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ sublanguage/include/admin-pagenode.php
@@ -0,0 +1,315 @@
+<?php 
+
+class Sublanguage_hierarchical_pages {
+	
+	/**
+	 * $translations[postID][language]
+	 * @from 1.0
+	 */
+	var $translations = array();
+	
+	/**
+	 * @from 1.0
+	 */
+	public function __construct() {
+		
+		// on generating rules (happens when flush)
+		add_action('generate_rewrite_rules', array($this, 'subpage_rewrite_rules'));
+		
+		// flush rule when updating page
+		add_action('post_updated', array($this, 'update_post'), null, 3);
+		
+		// flush rule when deleting page
+		add_action('before_delete_post', array($this, 'before_delete_post'));
+		
+		// add rewrite tag
+		add_action('admin_init', array($this, 'admin_init'));
+		
+	}
+	
+	/**
+	 *
+	 * @from 1.0
+	 */
+	function admin_init() {
+		
+		add_rewrite_tag('%nodepage_type%', '([^&]+)');
+		add_rewrite_tag('%nodepage_parent%', '([^&]+)');
+		add_rewrite_tag('%nodepage_path%', '([^&]+)');
+		add_rewrite_tag('%nodepage%', '([^&]+)');
+				
+	}
+	
+	
+	/**
+	 * rebuild permalink when saving post if parent/name has changed
+	 * Hook for 'post_updated'
+	 */
+	public function update_post($post_ID, $post_after, $post_before) {
+		global $sublanguage_admin;
+				
+		// only if post is hierarchical and translatable or translation
+		if ((in_array($post_after->post_type, $sublanguage_admin->options['cpt']) && is_post_type_hierarchical($post_after->post_type))
+			|| $sublanguage_admin->get_language_by_type($post_after->post_type) !== false) {
+			
+			// only if parent or name have changed
+			
+			if ($post_after->post_parent != $post_before->post_parent 
+				|| $post_after->post_name != $post_before->post_name
+				|| $post_after->post_status != $post_before->post_status) {
+				
+				$sublanguage_admin->disable_translate_home_url = true;
+				flush_rewrite_rules();
+				$sublanguage_admin->disable_translate_home_url = false;
+			}
+			
+		}
+		
+	}
+	
+	/**
+	 * rebuild permalink when saving post if parent/name has changed
+	 * Hook for 'post_updated'
+	 */
+	public function before_delete_post($post_id) {
+		global $sublanguage_admin;
+		
+		$post = get_post($post_id);
+		
+		if (in_array($post->post_type, $sublanguage_admin->options['cpt'])) {
+		
+			add_action('after_delete_post', array($this, 'after_delete_post'));
+		
+		}
+		
+	}
+	
+	/**
+	 * rebuild permalink when saving post if parent/name has changed
+	 * Hook for 'post_updated'
+	 */
+	public function after_delete_post($post_id) {
+		
+		$sublanguage_admin->disable_translate_home_url = true;
+		flush_rewrite_rules();
+		$sublanguage_admin->disable_translate_home_url = false;
+	
+	}
+	
+	
+	/**
+	 * @from 1.0
+	 */
+	public function subpage_rewrite_rules($rewrite) {
+		global $sublanguage_admin;
+		
+		$nodes = $this->get_page_nodes();
+		
+		if (count($nodes)) {
+		
+			$page_ids = array_keys($nodes);
+		
+			$sublanguage_admin->enqueue_translation($page_ids);
+			$sublanguage_admin->translate_queue();
+		
+			foreach ($nodes as $node) {
+		
+				if (count($node->children)) {
+				
+					$ancestors = $node->get_ancestors();
+					$ancestors = array_reverse($ancestors);
+				
+					$post_type = $node->page->post_type;
+					$post_type_obj = get_post_type_object($post_type);
+					$post_type_rewrite = $post_type_obj->rewrite;
+				
+					if ($post_type == 'page') {
+					
+						$path = '';
+					
+						if (!$post_type_rewrite) {
+					
+							$post_type_rewrite = array(
+								'with_front' => true,
+								'feeds' => false,
+								'pages' => true,
+								'ep_mask' => EP_PAGES
+							);
+					
+						}
+					
+					} else {
+					
+						$post_type_slug = $sublanguage_admin->get_cpt_translation($post_type, $sublanguage_admin->options['main']);
+					
+						$path = $post_type_slug.'/';
+
+					}
+								
+					foreach ($ancestors as $ancestor) {
+					
+						$path .= $ancestor->post_name.'/';
+				
+					}
+				
+					$path = rtrim($path, '/');
+				
+					$rewrite->add_rewrite_tag(
+						'%'.$path.'%', 
+						$path.'/([^/]+)', 
+						'nodepage_type='.$post_type.'&nodepage_parent='.$node->page->ID.'&nodepage_path='.$path.'&nodepage='
+					);
+				
+					$rules = $rewrite->generate_rewrite_rules(
+						'%'.$path.'%', 
+						$post_type_rewrite['ep_mask'], // ep_mask 
+						$post_type_rewrite['pages'], // paged  
+						false, // feed  
+						false, // forcomments  
+						false, // walk_dirs
+						true // endpoints
+					);
+				
+					$rewrite->rules = array_merge($rules, $rewrite->rules);
+				
+					//do_action('sublanguage_rewrite_nodepage', $node->page->ID, $post_type, $path, $post_type_rewrite);
+					
+					$languages = $sublanguage_admin->get_languages();
+					
+					foreach ($languages as $language) {
+				
+						if ($sublanguage_admin->get_post_translation($node->page->ID, $language->ID) !== false) {
+				
+							if ($post_type == 'page') {
+						
+								$translated_path = '';
+							
+							} else {
+							
+								$translated_path = $sublanguage_admin->get_cpt_translation($post_type, $language->ID).'/';
+						
+							}
+						
+							foreach ($ancestors as $ancestor) {
+
+								$translated_path .= $sublanguage_admin->translate_post_field($ancestor->ID, $language->ID, 'post_name', $ancestor->post_name) . '/';
+				
+							}
+						
+							$translated_path = rtrim($translated_path, '/');
+						
+							if ($translated_path != $path) {
+								
+								$rewrite->add_rewrite_tag( 
+									'%'.$translated_path.'%', 
+									$translated_path.'/([^/]+)', 
+									'nodepage_type='.$post_type.'&nodepage_parent='.$node->page->ID.'&nodepage_path='.$path.'&nodepage='
+								);
+							
+								$rules = $rewrite->generate_rewrite_rules(
+									'%'.$translated_path.'%', 
+									$post_type_rewrite['ep_mask'],  // ep_mask
+									$post_type_rewrite['pages'], 		// paged 
+									$post_type_rewrite['feeds'], 		// feed 
+									false, 													// forcomments 
+									false,													// walk_dirs
+									true														// endpoints
+								);
+							
+								$rewrite->rules = array_merge($rules, $rewrite->rules);
+
+							}
+						
+						}
+				
+					}
+				
+				}
+		
+			}
+			
+		}
+
+	}
+
+	
+	/**
+	 * @from 1.0
+	 */
+	public function get_page_nodes() {
+		global $wpdb, $sublanguage_admin;
+				
+		$hierarchical_post_types = array();
+		
+		foreach ($sublanguage_admin->options['cpt'] as $post_type) {
+		
+			$obj = get_post_type_object($post_type);
+			
+			if (isset($obj) && $obj->hierarchical && ($obj->_builtin || $obj->rewrite)) {
+			
+				$hierarchical_post_types[] = $post_type;
+				
+			}
+			
+		}
+		
+		$in_post_type = implode("','", esc_sql($hierarchical_post_types));
+
+		$hierarchical_posts = $wpdb->get_results(
+			"SELECT $wpdb->posts.* FROM $wpdb->posts
+			WHERE $wpdb->posts.post_type IN ('$in_post_type') AND $wpdb->posts.post_status = 'publish'"
+		);
+		
+		$nodes = array();
+	
+		foreach ($hierarchical_posts as $page) {
+		
+			$node = new Sublanguage_pagenode();
+			$node->page = $page;
+			$nodes[$page->ID] = $node;
+	
+		}
+		
+		foreach ($nodes as $node) {
+		
+			if ($node->page->post_parent && isset($nodes[$node->page->post_parent])) {
+			
+				$nodes[$node->page->post_parent]->children[] = $node;
+				$node->parent = $nodes[$node->page->post_parent];
+				
+			}
+		
+		}
+		
+		return $nodes;
+	
+	}
+
+	
+	
+}
+
+
+
+
+Class Sublanguage_pagenode {
+	
+	var $page;
+	var $children = array();
+	var $parent;
+	
+	public function get_ancestors($ancestors = array()) {
+		
+		array_push($ancestors, $this->page);
+		
+		if (isset($this->parent)) {
+			
+			$ancestors = $this->parent->get_ancestors($ancestors);
+		
+		} 
+		
+		return $ancestors;
+	}
+	
+}
+
---
+++ sublanguage/include/admin-permalink.php
@@ -0,0 +1,495 @@
+<?php 
+
+class Sublanguage_permalink {
+	
+	/**
+	 * @from 1.0
+	 */
+	var $permalink_translation = 'sublanguage_permalink';
+
+	/**
+	 * @from 1.0
+	 */
+	var $nonce = 'sublanguage_permalink_nonce';
+		
+	/**
+	 * @from 1.0
+	 */
+	var $action = 'sublanguage_permalink_action';
+	
+	/**
+	 * @from 1.0
+	 */
+	public function __construct() {
+		
+		add_action('admin_init', array($this, 'admin_init'));
+		
+	}
+	
+	/**
+	 *
+	 * @from 1.0
+	 */
+	function admin_init() {
+	
+		add_action( 'load-options-permalink.php', array($this, 'save_permalinks') );
+		
+		add_action( 'load-options-permalink.php', array($this, 'print_taxonomy_translation_form') );
+		add_action( 'load-options-permalink.php', array($this, 'print_cpt_translation_form') );
+		
+		add_filter('rewrite_rules_array', array($this, 'rewrite_rules'), 12);
+		
+		add_action('generate_rewrite_rules', array($this, 'rewrite_taxonomy'));
+		add_action('generate_rewrite_rules', array($this, 'rewrite_cpt'));
+		add_action('generate_rewrite_rules', array($this, 'rewrite_cpt_archive'));
+		
+		add_rewrite_tag('%sub_tax_term%', '([^&]+)');
+		add_rewrite_tag('%sub_tax_o%', '([^&]+)');
+		add_rewrite_tag('%sub_tax_t%', '([^&]+)');
+		add_rewrite_tag('%sub_tax_qv%', '([^&]+)');
+		
+		add_rewrite_tag('%sub_cpt_name%', '([^&]+)');
+		add_rewrite_tag('%sub_cpt_o%', '([^&]+)');
+		add_rewrite_tag('%sub_cpt_t%', '([^&]+)');
+		add_rewrite_tag('%sub_cpt_qv%', '([^&]+)');
+		
+	}
+	
+	/**
+	*	Filter for 'rewrite_rules_array'
+	*
+	* @from 1.0
+	*/
+	public function rewrite_rules($rules) {
+		global $sublanguage_admin;
+		
+		$slugs = $sublanguage_admin->get_language_column('post_name');
+		
+		if ($slugs) {
+			
+			$new_rules = array();
+			
+			$new_rules['(?:'.implode('|', $slugs).')/?$'] = 'index.php';
+		
+			foreach ($rules as $key => $val) {
+			
+				$key = '(?:'.implode('/|', $slugs).'/)?' . $key;
+			
+				$new_rules[$key] = $val;
+			
+			}
+			
+			return $new_rules;
+				
+		}
+	
+		return $rules;
+		
+	}
+	
+
+	
+	/**
+	 *
+	 * @from 1.0
+	 */
+	public function save_permalinks() {
+		global $sublanguage_admin;
+		
+		if (isset($_POST[$this->nonce]) 
+			&& wp_verify_nonce($_POST[$this->nonce], $this->action)
+			&& isset($_POST[$this->permalink_translation])) {
+			
+			$translations = get_option($sublanguage_admin->translation_option_name);
+					
+			foreach ($_POST[$this->permalink_translation] as $type => $data) {
+				
+				foreach ($data as $language_id => $translation_data) {
+							
+					foreach ($translation_data as $original => $translation) {
+						
+						if (($type == 'taxonomy' || $type == 'cpt') 
+							&& $sublanguage_admin->get_language_by($language_id, 'ID') !== false
+							&& in_array($original, $sublanguage_admin->options[$type])) {
+								
+							$translations[$type][$language_id][$original] = sanitize_title($translation);
+								
+						}
+					
+					}
+				
+				}
+			
+			}
+			
+			update_option($sublanguage_admin->translation_option_name, $translations);
+			
+		}
+		
+	}
+	
+	/**
+	 * Add a section in settings>permalink to translate taxonomies
+	 *
+	 * @from 1.0
+	 */
+	public function print_taxonomy_translation_form() {
+		global $sublanguage_admin;
+		
+		if (isset($sublanguage_admin->options['taxonomy']) && $sublanguage_admin->options['taxonomy']) {
+			
+			foreach ($sublanguage_admin->options['taxonomy'] as $taxonomy_name) {
+			
+				$taxonomy = get_taxonomy($taxonomy_name);
+				
+				if (isset($taxonomy) && $taxonomy->rewrite) {
+				
+					add_settings_section(
+						'sublanguage_taxis_section', 
+						__('Translate taxonomies', 'sublanguage'), 
+						array($this, 'add_taxonomy_section'), 
+						'permalink' 
+					);
+					
+					break;
+				}
+				
+			}
+			
+			foreach ($sublanguage_admin->options['taxonomy'] as $taxonomy_name) {
+			
+				$taxonomy = get_taxonomy($taxonomy_name);
+				
+				if (isset($taxonomy) && $taxonomy->rewrite) {
+				
+					add_settings_field( 
+						'sublanguage_'.$taxonomy_name, 
+						isset($taxonomy->labels->name) ? $taxonomy->labels->name : $taxonomy_name, 
+						array($this, 'add_field'), 
+						'permalink', 
+						'sublanguage_taxis_section', 
+						array(
+							'name' => $taxonomy_name, 
+							'slug' => $taxonomy->rewrite['slug'],
+							'type' => 'taxonomy'
+						) 
+					);
+					
+				}
+			
+			}
+		
+		}
+		
+	}
+
+
+	/**
+	 * Add a section in settings>permalink to translate taxonomies
+	 *
+	 * @from 1.0
+	 */
+	function print_cpt_translation_form() {
+		global $sublanguage_admin;
+		
+		if (isset($sublanguage_admin->options['cpt']) && $sublanguage_admin->options['cpt']) {
+			
+			foreach ($sublanguage_admin->options['cpt'] as $cpt_name) {
+			
+				$cpt = get_post_type_object($cpt_name);
+					
+				if (isset($cpt) && $cpt->rewrite) {
+				
+					add_settings_section(
+						'sublanguage_cpt_section', 
+						__('Translate Custom Post Types', 'sublanguage'), 
+						array($this, 'add_cpt_section'), 
+						'permalink' 
+					);
+					break;
+					
+				}
+			
+			}
+		
+			foreach ($sublanguage_admin->options['cpt'] as $cpt_name) {
+			
+				$cpt = get_post_type_object($cpt_name);
+				
+				if (isset($cpt) && $cpt->rewrite) {
+				
+					add_settings_field( 
+						'sublanguage_'.$cpt_name, 
+						isset($cpt->labels->name) ? $cpt->labels->name : $cpt_name, 
+						array($this, 'add_field'), 
+						'permalink', 
+						'sublanguage_cpt_section', 
+						array(
+							'name' => $cpt_name, 
+							'slug' => $cpt->rewrite['slug'],
+							'type' => 'cpt'
+						) 
+					);
+					
+				}
+				
+			}
+			
+		}
+		
+	}
+
+	
+	/**
+	 * Add a section in settings>permalink
+	 *
+	 * @from 1.0
+	 */
+	function add_taxonomy_section() {
+		
+		wp_nonce_field($this->action, $this->nonce, false);
+				
+		echo __('You can enter translations for taxonomies. These translations will be used in the url of category/tag/term pages or taxonomy archive pages. If you leave these blank the defaults will be used.', 'sublanguage');
+		
+		
+		
+		return;
+		
+	}
+	
+	/**
+	 * @from 1.0
+	 */
+	function add_cpt_section() {
+						
+		echo __('You can enter translations for custom post-types. These translations will be used in the url of custom post-type pages or custom post-type archive pages. If you leave these blank the defaults will be used.', 'sublanguage');
+
+		return;
+		
+	}
+
+	/**
+	 * Add a field in section for custom-post-types
+	 *
+	 * @from 1.0
+	 */
+	function add_field($args) {
+		global $sublanguage_admin;
+		
+		$translations = get_option($sublanguage_admin->translation_option_name);
+		$languages = $sublanguage_admin->get_languages();
+		
+		$original = $args['name'];
+		$slug = $args['slug'];
+		$type = $args['type'];
+			
+   		foreach ($languages as $language) {
+						
+			if (isset($translations[$type][$language->ID][$original]) && $translations[$type][$language->ID][$original]) {
+			
+				$value = $translations[$type][$language->ID][$original];
+				
+			} else {
+				
+				$value = '';
+			
+			}
+			
+			echo '<label><input type="text" value="'.$value.'" name="'.$this->permalink_translation.'['.$type.']['.$language->ID.']['.$original.']" class="regular-text" />
+				'.$language->post_title.'</label><br />';
+			
+		}
+		
+		echo '<p>'.sprintf(__('default: %s'), '<code>'.$slug.'</code>').'</p>';
+		
+		
+	}
+	
+	
+	/**
+	 * @from 1.0
+	 */
+	function rewrite_taxonomy($rewrite) {
+		global $sublanguage_admin;
+		
+		$translations = get_option($sublanguage_admin->translation_option_name);
+		
+		if (isset($translations['taxonomy']) && $translations['taxonomy']) {
+			
+			foreach($translations['taxonomy'] as $language_id => $data) {
+			
+				foreach($data as $original => $translation) {
+				
+					$taxonomy_obj = get_taxonomy($original);
+				
+					if (in_array($original, $sublanguage_admin->options['taxonomy']) 
+						&& isset($taxonomy_obj->query_var, $taxonomy_obj->rewrite) 
+						&& $taxonomy_obj->rewrite) {
+						
+						if ($translation != '' && $translation != $taxonomy_obj->rewrite['slug']) {
+						
+							$rewrite->add_rewrite_tag(
+								'%'.$translation.'%', 
+								$translation.'/([^/]+)', 
+								'sub_tax_o='.$original.'&sub_tax_qv='.$taxonomy_obj->query_var.'&sub_tax_t='.$translation.'&sub_tax_term='
+							);
+						
+							$rules = $rewrite->generate_rewrite_rules(
+								'%'.$translation.'%', 
+								$taxonomy_obj->rewrite['ep_mask'], // ep_mask
+								true, // paged
+								true, // feed
+								false, // forcomments
+								false, // walk_dirs
+								true // endpoints
+							);
+					
+							$rewrite->rules = array_merge($rules, $rewrite->rules);
+						
+						}
+				
+					}
+					
+				}
+			
+			}
+			
+		}
+		
+		
+	}
+	
+	/**
+	 * @from 1.0
+	 */
+	function rewrite_cpt($rewrite) {
+		global $sublanguage_admin;
+		
+		$translations = get_option($sublanguage_admin->translation_option_name);
+		
+		if (isset($translations['cpt']) && $translations['cpt']) {
+			
+			foreach($translations['cpt'] as $language_id => $items) {
+				
+				foreach($items as $original => $translation) {
+				
+					$post_type_obj = get_post_type_object($original);
+				
+					if (isset($post_type_obj->query_var, $post_type_obj->rewrite) && $post_type_obj->rewrite) {
+					
+						$post_type_rewrite = $post_type_obj->rewrite;
+						$regex_cpt = '';
+											
+						if ($translation != '' && $translation != $post_type_obj->rewrite['slug']) {
+							
+							$regex_cpt .= $translation . '|'; 
+							
+							$rewrite->add_rewrite_tag(
+								'%'.$translation.'%', 
+								$translation.'/([^/]+)', 
+								'sub_cpt_o='.$original.'&sub_cpt_qv='.$post_type_obj->query_var.'&sub_cpt_t='.$translation.'&sub_cpt_name='
+							);
+							
+							$rules = $rewrite->generate_rewrite_rules(
+								'%'.$translation.'%', 
+								$post_type_rewrite['ep_mask'], // ep_mask
+								$post_type_rewrite['pages'], // paged
+								false, // feed
+								true, // forcomments
+								false, // walk_dirs
+								true // endpoints
+							);
+						
+							$rewrite->rules = array_merge($rules, $rewrite->rules);
+					
+						}
+						
+					}
+						
+				}
+		
+			}
+			
+		}
+		
+		
+	}
+	
+	
+	/**
+	 * @from 1.0
+	 */
+	function rewrite_cpt_archive($rewrite) {
+		global $sublanguage_admin;
+		
+		$translations = get_option($sublanguage_admin->translation_option_name);
+			
+		if (isset($translations['cpt']) && $translations['cpt']) {
+			
+			$sorted_translations = array();
+			
+			foreach($translations['cpt'] as $language_id => $items) {
+				
+				foreach($items as $original => $translation) {
+				
+					$sorted_translations[$original][$language_id] = $translation;
+					
+				}
+				
+			}
+			
+			foreach($sorted_translations as $original => $item) {
+			
+				$post_type_obj = get_post_type_object($original);
+			
+				if (isset($post_type_obj) && $post_type_obj->has_archive) {
+					
+					$post_type_rewrite = $post_type_obj->rewrite;
+					$regex_cpt = '';
+				
+					foreach($item as $language_id => $translation) {
+											
+						if ($translation != '' && $translation != $post_type_obj->rewrite['slug']) {
+							
+							$regex_cpt .= $translation . '|'; 
+							
+						}
+						
+					}
+					
+					if ($regex_cpt) {
+						
+						$regex_cpt = rtrim($regex_cpt, '|');
+									
+						$rewrite->add_rewrite_tag(
+							'%'.$regex_cpt.'%', 
+							'('.$regex_cpt.')', 
+							'sub_cpt_o='.$original.'&sub_cpt_qv='.$post_type_obj->query_var.'&sub_cpt_t='
+						);
+		
+						$rules = $rewrite->generate_rewrite_rules(
+							'%'.$regex_cpt.'%', 
+							$post_type_rewrite['ep_mask'], // ep_mask
+							$post_type_rewrite['pages'], // paged
+							$post_type_rewrite['feeds'], // feed
+							false, // forcomments
+							false, // walk_dirs
+							true // endpoints
+						);
+					
+						$rewrite->rules = array_merge($rules, $rewrite->rules);						
+					
+					}
+						
+				}
+		
+			}
+			
+		}
+		
+	}
+	
+	
+	
+			
+}
