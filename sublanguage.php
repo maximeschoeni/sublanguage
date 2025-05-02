17945418c937c5535dab080be501d85f64a4306c
Julien-ROBERT LECADOU <hartou@users.noreply.github.com>
2025-05-02 10:55:07 +0000

    bump version to 2.11 and update changelog for recent changes and bug fixes

--- sublanguage.php
+++ sublanguage.php
@@ -4,7 +4,7 @@ Plugin Name: Sublanguage
 Plugin URI: http://sublanguageplugin.wordpress.com
 Description: Plugin for building a site with multiple languages
 Author: Maxime Schoeni
-Version: 2.10
+Version: 2.11
 Author URI: http://sublanguageplugin.wordpress.com
 Text Domain: sublanguage
 Domain Path: /languages
